4b95b13aa34ccfcfec0744fc452415db3c718c64
N3ith <alisasava@web.de>
2016-05-01 14:55:22 +0200

    Added files via upload

--- main.js
+++ main.js
@@ -1,41 +1,35 @@
+//Aufgabe 1 
+
 //Node.js bietet das Modul "fs" an, welches den Zugriff auf Dateien ermöglicht -> var fs = require('fs')
-// var chalk = require ('chalk') -> chalk -> damit kann man Buchstarben einfärben -> 1 Schritt -> die Installation npm install --save chalk z.b. kann man mit der Eingabe -> chalk.blue('Hello world!') -> kann man eine Zeichenkette färben
-// wofür steht nun "var" -> In JavaScrip deklariert man eine Variable via "var Statement" bevor man es benutzt  
+// wofür steht nun "var" -> In JavaScrip deklariert man eine Variable via "var Statement" bevor man es benutzt 
 
 var fs = require('fs');
-var chalk = require('chalk');
 
 //Die Funktion -> fs.readFile(__dirname+"/wolkenkratzer.json",function(err,data)) -> ermöglicht das asynchrone Auslesen von Dateien
 // __dirname ->  enthält Name des Verzeichnisses , in dem das aktuelle Programm liegt.
+//err -> error
+// Der Parameter -> data ist normalerweise ein Buffer mit Binärdaten, der mittels data.toString() in einen String umgewandelt werden kann.
 
 fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) { 
 
 		if (err) throw err;
+    
+        // die Javascript Funktion JSON.parse(text) wird benutzt um einen JSON text  in ein javascript objekt umzuwandeln
+        var emulated = JSON.parse(data.toString());
 
-			var emulated = JSON.parse(data.toString());
-
-			emulated.wolkenkratzer.sort(function(a,b) {
-
-				if (a.hoehe > b.hoehe) {
-   					return 1;
-  				}
-  				if (a.hoehe < b.hoehe) {
-    				return -1;
-  				}
-  				// a muss gleich sein mit b 
-  					return 0;
-				}
-			);
-
-		fs.writeFile(__dirname+"/sorted_wolkenkratzer.json", JSON.stringify(emulated), function(err) {
+    //hatte da vorher dirname+ data stehen dadurch hat es einfach die unveränderte json datei ausgegeben
+    //damit konnte ich die wolkenkratzer datei verändern 
+    //fs.writeFile -> ermöglicht das asynchrone Schreiben von Dateien
+    fs.writeFile(__dirname+"/wolkenkratzer.json",function(err) {
 
 			if (err) throw err;
-
-			for(var i in emulated.wolkenkratzer){
-				console.log(chalk.green("Name:" + emulated.wolkenkratzer[i].name));
-				console.log(chalk.yellow("Stadt:" + emulated.wolkenkratzer[i].stadt));
-				console.log(chalk.red("Hoehe:" + emulated.wolkenkratzer[i].hoehe));
-				console.log("--------------------------------------");
-			}
-		});
- });
+        
+            //Beispiel einer for-Schleife in JSON -> for(var k in result) {console.log(k, result[k]);}
+            for(var i in emulated.wolkenkratzer){
+            console.log("Name:" + emulated.wolkenkratzer[i].name);
+            console.log("Stadt:" + emulated.wolkenkratzer[i].stadt);
+            console.log("Hoehe:" + emulated.wolkenkratzer[i].hoehe);
+            console.log("--------------------------------------");
+            }
+    });
+});
